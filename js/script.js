94ab46fe5c052769dae5999463b121edfd83d500
Evan Scully <98612784+ScullyAerialPhotography@users.noreply.github.com>
2023-05-06 00:05:31 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,6 +7,10 @@
 // function nextSection() {
 //     if(document.body.scr)
 // }
+function currentYear() {
+    let d = new Date();
+    document.getElementById('copyright').innerHTML = d.getFullYear();
+}
 
 // JS FOR ARROW BUTTON IN .one
 window.onclick = function() {nextSection()};
@@ -25,8 +29,58 @@ function scrollFunction() {
         document.getElementById("header").style.fontSize = "100%"
     }
 }
-// JS FOR FOOTER DATE
-window.onload = function() {
-    var d = new Date();
-    document.getElementById("time").innerHTML = d.getFullYear();
+
+let intIndex = 0;
+
+function showImage(i) {
+// function showImage() {
+    // intImage++;
+    intIndex += i;
+
+    let images = document.getElementsByClassName('image');
+    let dots = document.getElementsByClassName('dot');
+
+    for(k = 0; k<images.length; k++) {
+        images[k].style.display = "none";
+        dots[k].className = dots[k].className.replace("active", "");
+    }
+    //go back to first image if intIndex is 0
+    if(intIndex > images.length - 1) {
+        intIndex = 0
+    }
+    if(intIndex < 0) {
+        intIndex = images.length-1;
+    }
+
+    images[intIndex].style.display = "block";
+    dots[intIndex].className += " active"
+
 }
+
+// When the user scrolls down 800px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    document.getElementById("cornerBtn").style.display = "block";
+  } else {
+    document.getElementById("cornerBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+//MAIN PRG
+window.onload = function() { //live when on page
+    currentYear();
+    showImage(intIndex);
+}
+
+
